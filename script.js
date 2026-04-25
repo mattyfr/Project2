b2d6fe37113d077065237d2dffc32310ad96bd72
Mathias Granlund <granlund.jr@gmail.com>
2026-04-25 22:27:45 +0200

    Started with CSS and fixed some problems in JS like price not being shown

--- script.js
+++ script.js
@@ -1,27 +1,32 @@
-let body = document.getElementById("body")
-let displayPlace = document.getElementById("displayItemPlace")
-let List = []
-for(let i = 0; i < 20; i++){
+load()
+
+
+function load() {
+  let body = document.getElementById("body");
+  let displayPlace = document.getElementById("displayItemPlace");
+  let List = [];
+  for (let i = 0; i < 20; i++) {
     let item = {
-        name: "item",
-        price: 10,
-        id: i,
-    }
-    List.push(item)
+      name: "item",
+      price: 10 + i,
+      id: i,
+    };
+    List.push(item);
+  }
+  window.localStorage.setItem("items", JSON.stringify(List));
+  let items = window.localStorage.getItem("items");
+  items = JSON.parse(items);
+
+  let i = 0;
+  items.forEach((element) => {
+    i++;
+    let div = document.createElement("div");
+    let img = document.createElement("img");
+    img.src = "ak_inheritance.webp";
+    let buyButton = document.createElement("button");
+    buyButton.innerText = "Buy " + items[i].price;
+    displayPlace.appendChild(div);
+    div.appendChild(img);
+    div.appendChild(buyButton);
+  });
 }
-window.localStorage.setItem("items", JSON.stringify(List))
-let items = window.localStorage.getItem("items")
-items = JSON.parse(items)
-
-
-items.forEach(element => {
-    let div = document.createElement("div")
-    let img = document.createElement("img")
-    img.src = "ak_inheritance.webp"
-    let buyButton = document.createElement("button")
-    buyButton.textContent = items.price
-    displayPlace.appendChild(div)
-    div.appendChild(img)
-    div.appendChild(buyButton)
-    console.log("a");
-});
