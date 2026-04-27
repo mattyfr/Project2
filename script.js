3a1937b34fac3b0c456eb19d8147e740658c0fb0
Mathias Granlund <granlund.jr@gmail.com>
2026-04-27 11:00:04 +0200

    Working on making it look more like the plan

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 load()
-
+loadSearchBar()
 
 function load() {
-  let body = document.getElementById("body");
-  let displayPlace = document.getElementById("displayItemPlace");
-  let List = [];
-  for (let i = 0; i < 20; i++) {
-    let item = {
+    let body = document.getElementById("body");
+    let displayPlace = document.getElementById("displayItemPlace");
+    let List = [];
+    for (let i = 0; i < 20; i++) {
+        let item = {
       name: "item",
       price: 10 + i,
       id: i,
@@ -16,17 +16,31 @@ function load() {
   window.localStorage.setItem("items", JSON.stringify(List));
   let items = window.localStorage.getItem("items");
   items = JSON.parse(items);
-
+  
   let i = 0;
   items.forEach((element) => {
-    i++;
-    let div = document.createElement("div");
-    let img = document.createElement("img");
-    img.src = "ak_inheritance.webp";
-    let buyButton = document.createElement("button");
-    buyButton.innerText = "Buy " + items[i].price;
-    displayPlace.appendChild(div);
-    div.appendChild(img);
-    div.appendChild(buyButton);
+      let div = document.createElement("div");
+      let img = document.createElement("img");
+      img.src = "ak_inheritance.webp";
+      let buyButton = document.createElement("button");
+      buyButton.innerText = "Buy " + items[i].price;
+      displayPlace.appendChild(div);
+      div.appendChild(img);
+      div.appendChild(buyButton);
+      i++;
   });
+}
+function loadSearchBar() {
+    let searchPlace = document.getElementById("search")
+    let title = document.createElement("h1")
+    title.textContent = "Searchbar"
+    let div = document.createElement("div");
+    let text1 = document.createElement("p")
+    text1.textContent = "name"
+    let input1 = document.createElement("input")
+    searchPlace.appendChild(div)
+    div.appendChild(title)
+    div.appendChild(text1)
+    div.appendChild(input1)
+
 }
